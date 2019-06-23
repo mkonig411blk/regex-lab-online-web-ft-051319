0b757418f23cd892db80024cd2f881aec455ebed
Margot Konig <margot.konig@blackrock.com>
2019-06-23 01:13:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,20 @@
 def starts_with_a_vowel?(word)
-if word.match(/^[aeiou]/i) != nil
-   true
-else false
-end
+  word.match(/^[aeiou]/i) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/un\w*ing/)
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- first_letter = text.scan(/\w+/)[0]
-(text.scan(/\./)) && (first_letter == first_letter.upcase)
+  first_letter = text.scan(/\w+/)[0]
+  (text.scan(/\./)) && (first_letter == first_letter.upcase)
 end
 
 def valid_phone_number?(phone)
-phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+  phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
 end
